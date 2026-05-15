b1797c0c3691fa56352beac717a8de4b76ea0eca
Ocepek-Domen <do2898@student.uni-lj.si>
2026-05-15 22:16:18 +0200

    removed login/register

---
+++ resources/js/pages/marketing/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Implementor</h1>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::inertia('/', 'marketing/Home')->name('home');
+Route::inertia('/services', 'marketing/Services')->name('services');
+Route::inertia('/clients', 'marketing/Clients')->name('clients');
+Route::inertia('/clients/ilirika', 'marketing/clients/Ilirika')->name('clients.ilirika');
+Route::inertia('/clients/nn-saunas', 'marketing/clients/NnSaunas')->name('clients.nn-saunas');
+Route::inertia('/clients/mobistekla', 'marketing/clients/Mobistekla')->name('clients.mobistekla');
+Route::inertia('/about', 'marketing/About')->name('about');
+Route::inertia('/contact', 'marketing/Contact')->name('contact');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
